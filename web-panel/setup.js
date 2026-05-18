164ca64f6c071e0c87bf849692643e14819ebd42
Reikiiioi <161363074+Reikiiioi@users.noreply.github.com>
2026-05-18 13:39:33 +0300

    Implement bcryptjs installation check in setup.js
    Added dependency check and installation for bcryptjs.

--- web-panel/setup.js
+++ web-panel/setup.js
@@ -1,16 +1,28 @@
+const { execSync } = require('child_process');
 const readline = require('readline');
-const bcrypt = require('bcryptjs');
-const crypto = require('crypto');
 const fs = require('fs');
 const path = require('path');
+
+const configPath = path.join(__dirname, 'config.json');
+const config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
+
+console.log('[*] Проверка зависимостей...');
+if (!fs.existsSync(path.join(__dirname, 'node_modules', 'bcryptjs'))) {
+  console.log('[*] Установка bcryptjs...');
+  execSync('npm install', { cwd: __dirname, stdio: 'inherit' });
+  console.log('[OK] bcryptjs установлен.');
+} else {
+  console.log('[OK] bcryptjs уже установлен.');
+}
+console.log('');
+
+const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-
-const configPath = path.join(__dirname, 'config.json');
-const config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
 
 rl.question('Введи новый пароль для панели: ', (password) => {
   if (!password || password.length < 3) {
